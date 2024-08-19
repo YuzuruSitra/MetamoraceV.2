6b47a57dce28565a8f03f05f111fef140fb28334
Sitra <yuzurusitra@gmail.com>
2024-08-20 04:33:05 +0900

    ブロックの予測線実装

--- Assets/Scripts/System/Battle/BlockGenerator.cs
+++ Assets/Scripts/System/Battle/BlockGenerator.cs
@@ -1,13 +1,17 @@
+using System.Collections;
 using Photon.Pun;
 using UnityEngine;
 
 namespace System.Battle
 {
-    public class BlockGenerator : MonoBehaviour
+    public class BlockGenerator : MonoBehaviourPunCallbacks
     {
+        private const int MaxGenerate = 3;
+        private const int AllGenerate = MaxGenerate * 2;
         [Header("Index-TeamNum")]
         [SerializeField] private GameObject[] _blockTeam;
         [SerializeField] private Vector3[] _insPos;
+        [SerializeField] private Vector3[] _generatePos = new Vector3[AllGenerate];
         [SerializeField] private Vector3[] _insRot;
         [SerializeField] private Transform[] _parentObj;
         [SerializeField] private float _insInterVal;
@@ -16,11 +20,29 @@ namespace System.Battle
         private const int FieldSize = 120;
         private const float RayDistance = 1.0f;
         private float _currentTime;
-        private const int MaxGenerate = 3;
         public int BlocksShareTeam1 => CalcBlocksShare(_parentObj[0]);
         public int BlocksShareTeam2 => CalcBlocksShare(_parentObj[1]);
-        
         [SerializeField] private TimeHandler _timeHandler;
+        
+        [SerializeField] private GameObject _predictPrefab;
+        private readonly GameObject[] _predictObjs = new GameObject[AllGenerate];
+        private readonly Vector3[] _predictPos = new Vector3[AllGenerate];
+        private const float PredictPosY = 5.8f;
+        [SerializeField] private int _toggleCount;
+        [SerializeField] private float _toggleTime;
+        private WaitForSeconds _toggleWait;
+        private void Start()
+        {
+            _toggleWait = new WaitForSeconds(_toggleTime);
+            for (var i = 0; i < AllGenerate; i++)
+            {
+                _predictObjs[i] = Instantiate(_predictPrefab, Vector3.zero, Quaternion.identity);
+                _predictPos[i].y = PredictPosY;
+            }
+            if (!PhotonNetwork.IsMasterClient) return;
+            for (var i = 0; i < _generatePos.Length; i++)
+                _generatePos[i].y = _insPos[0].y;
+        }
 
         private void Update()
         {
@@ -29,27 +51,66 @@ namespace System.Battle
             if (_timeHandler.BattleTime <= 0) return;
             _currentTime += Time.deltaTime;
             if (_currentTime <= _insInterVal) return;
-            InsBlock();
+            SetPositionInfo();
             _currentTime = 0;
         }
 
-        private void InsBlock()
+        private void SetPositionInfo()
         {
-            var insCount = UnityEngine.Random.Range(1,  MaxGenerate + 1);
-            for (var i = 0; i < _blockTeam.Length; i++)
+            var teamInsCount = UnityEngine.Random.Range(1,  MaxGenerate + 1);
+            var allInsCount = teamInsCount * 2;
+            for (var i = 0; i < allInsCount; i++)
             {
+                int calcPosX;
+                do
+                {
+                    calcPosX = UnityEngine.Random.Range(MinPosX, MaxPosX);
+                } while (Mathf.Approximately(calcPosX, _generatePos[i].x) || ObjectExistsInRay(_generatePos[i], calcPosX));
+                _generatePos[i].x = calcPosX;
+                _generatePos[i].z = i < teamInsCount ? _insPos[0].z : _insPos[1].z;
+                _predictPos[i].x = _generatePos[i].x;
+                _predictPos[i].z = _generatePos[i].z;
+            }
+            photonView.RPC(nameof(CallGenerate), RpcTarget.All, _predictPos, _generatePos, allInsCount);
+        }
+        
+        [PunRPC]
+        private void CallGenerate(Vector3[] predictPos, Vector3[] generatePos, int insCount)
+        {
+            StartCoroutine(GenerateBlocks(predictPos, generatePos, insCount));
+        }
+
+        private IEnumerator GenerateBlocks(Vector3[] predictPos, Vector3[] generatePos, int insCount)
+        {
+            yield return ToggleActiveObjects(predictPos, insCount);
+            
+            if (!PhotonNetwork.IsMasterClient) yield break;
+            InstantiateBlocks(insCount, generatePos);
+        }
+        
+        private IEnumerator ToggleActiveObjects(Vector3[] predictPos, int insCount)
+        {
+            for (var i = 0; i < insCount; i++)
+                _predictObjs[i].transform.position = predictPos[i];
+            
+            for (var i = 0; i < _toggleCount; i++)
+            {
+                var setActive = i % 2 == 0;
                 for (var j = 0; j < insCount; j++)
                 {
-                    int calcPosX;
-                    do
-                    {
-                        calcPosX = UnityEngine.Random.Range(MinPosX, MaxPosX);
-                    } while (Mathf.Approximately(calcPosX, _insPos[i].x) || ObjectExistsInRay(_insPos[i], calcPosX));
-
-                    _insPos[i].x = calcPosX;
-                    var obj = PhotonNetwork.Instantiate(_blockTeam[i].name, _insPos[i], Quaternion.Euler(_insRot[i]));
-                    obj.transform.SetParent(_parentObj[i]);
+                    _predictObjs[j].SetActive(setActive);
                 }
+                yield return _toggleWait;
+            }
+        }
+        
+        private void InstantiateBlocks(int insCount, Vector3[] generatePos)
+        {
+            for (var i = 0; i < insCount; i++)
+            {
+                var teamNum = i < insCount / 2 ? 0 : 1;
+                var obj = PhotonNetwork.Instantiate(_blockTeam[teamNum].name, generatePos[i], Quaternion.Euler(_insRot[teamNum]));
+                obj.transform.SetParent(_parentObj[teamNum]);
             }
         }
         
