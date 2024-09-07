b4beb164b2f7e0c0e237b19e4ce6de6ca36b4ed0
kaikai-git <kaiandika@icloud.com>
2024-09-08 04:02:34 +0900

    名前表示

--- Assets/Scripts/Effect/BillBoard.cs
+++ Assets/Scripts/Effect/BillBoard.cs
@@ -2,12 +2,18 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BillBoard : MonoBehaviour {
+public class BillBoard : MonoBehaviour
+{
     [SerializeField] bool NotUseBillboard;
-    void Update () {
-      
-            Vector3 p = Camera.main.transform.position;
-            p.y = transform.position.y;
-            transform.LookAt (p);
+
+    void Update()
+    {
+        if (!NotUseBillboard)
+        {
+            Vector3 cameraPosition = Camera.main.transform.position;
+            Vector3 direction = cameraPosition - transform.position;
+            direction.y = 0; // Y軸方向の回転を防ぐ
+            transform.rotation = Quaternion.LookRotation(-direction); // 反転を防ぐために方向を反転
+        }
     }
 }
