47258e814d086f5cca9c1e902b70b931059c9366
kaikai-git <kaiandika@icloud.com>
2024-10-20 13:27:55 +0900

    ⑥⑦アイテムと決闘info追加

--- Assets/Scripts/System/UI/InfoPanelHandler.cs
+++ Assets/Scripts/System/UI/InfoPanelHandler.cs
@@ -9,10 +9,14 @@ namespace System.UI
         [SerializeField] private Button _infoRuleBt;
         [SerializeField] private Button _infoHowToBt;
         [SerializeField] private Button _infoStoryBt;
+        [SerializeField] private Button _infoItemBt;
+        [SerializeField] private Button _infoDuelBt;
         
         [SerializeField] private GameObject _rulePanel;
         [SerializeField] private GameObject _howToPanel;
         [SerializeField] private GameObject _storyPanel;
+        [SerializeField] private GameObject _itemPanel;
+        [SerializeField] private GameObject _duelPanel;
         private UiSeHandler _uiSeHandler;
         
         private void Start()
@@ -21,6 +25,8 @@ namespace System.UI
             _infoRuleBt.onClick.AddListener(InfoRule);
             _infoHowToBt.onClick.AddListener(InfoHowTo);
             _infoStoryBt.onClick.AddListener(InfoStory);
+            _infoItemBt.onClick.AddListener(InfoItem);
+            _infoDuelBt.onClick.AddListener(InfoDuel);
         }
 
         private void InfoRule()
@@ -44,11 +50,25 @@ namespace System.UI
             _uiSeHandler.PushSound();
         }
 
+        private void InfoItem()
+        {
+            AllCloseInfoPanel();
+            _itemPanel.SetActive(true);
+            _uiSeHandler.PushSound();
+        }
+        private void InfoDuel()
+        {
+            AllCloseInfoPanel();
+            _duelPanel.SetActive(true);
+            _uiSeHandler.PushSound();
+        }
         private void AllCloseInfoPanel()
         {
             _rulePanel.SetActive(false);
             _howToPanel.SetActive(false);
             _storyPanel.SetActive(false);
+            _itemPanel.SetActive(false);
+            _duelPanel.SetActive(false);
         }
     }
 }
